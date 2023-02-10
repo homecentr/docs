d5e2113e99bb6490bfe92869b818eaca6683ccca
Lukas Holota <lholota@vistaprint.com>
2023-02-10 16:29:45 +0100

    Sitemap link fixed

--- src/consts.ts
+++ src/consts.ts
@@ -4,15 +4,6 @@ export const SITE = {
 	defaultLanguage: 'en-us',
 } as const;
 
-export const OPEN_GRAPH = {
-	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
-	}
-};
-
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
 	indexName: 'XXXXXXXXXX',
