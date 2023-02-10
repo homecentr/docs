256214d1f48cf8973a52bc5c726e69fef17789d4
Lukas Holota <lholota@vistaprint.com>
2023-02-10 16:16:53 +0100

    Broken links fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import {
 } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     // Enable Preact to support Preact JSX components.
     preact(),
     // Enable React for the Algolia search component.
     react(),
-    mdx()
+    mdx(),
+    sitemap()
   ],
-  site: `https://docs.homecentr.io`
+  site: `https://docs.homecentr.one`
 });
